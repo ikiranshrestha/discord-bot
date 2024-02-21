73e7f2da4072bc5a48e132864c0a9ed740ac1f0d
ikiranshrestha <kiranshrestha903@gmail.com>
2024-02-21 14:25:58 +0545

    git: project initialization

---
+++ config/app.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'name' => 'Laracord',
+  'version' => 'unreleased',
+  'env' => 'production',
+  'timezone' => 'UTC',
+  'providers' => 
+  array (
+    0 => 'App\\Providers\\BotServiceProvider',
+  ),
+);
